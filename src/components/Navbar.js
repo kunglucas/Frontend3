9e717814ad910966053995cf9a249c2000844a57
gufr2201 <gfranchell@gmail.com>
2023-01-02 07:49:41 +0100

    Addcommments about Link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
-import './Navbar.component.scss';
+ import './Navbar.component.scss';
+//  import { Link } from "react-router-dom"; Denna måste vi ha för att kunna använda link
+
 const Navbar = () => {
     return (
       <nav className="navbar">
         <h1>Lucas & Gustav</h1>
         <div className="links">
-          <a href="/">Home</a>
+           <a href="/">Home</a> 
+           {/* Vi bör ändra länkarna till: <Link to="/">Home</Link> och samma nedan */} 
           <a href="/create">Add user</a>
         </div>
       </nav>

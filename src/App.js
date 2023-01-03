9e61104d32c60835ade62797f61e18bdd0a0c19e
Lucas Larsson <lucas.larsson93@hotmail.com>
2023-01-03 09:40:27 +0100

    filter table by search

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Form from './components/Form';
--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,14 +1,22 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+
 const BlogList = ({ blogs }) => {
+  const [search, setSearch] = useState('')
   return (
-    <table>
-    <tr>
+    <table id="tableData">
+    <input placeholder="Search table.." type="text" onChange={(e) => setSearch(e.target.value)} />
+   <tr>
       <th>Name</th>
       <th>Age</th>
       <th>Email</th>
       <th>Type</th>
     </tr>
-      {blogs.map(blog => (
+      {blogs.filter((blog) => {
+        return search.toLowerCase() === '' ? blog : blog.name.toString().toLowerCase().includes(search.toLowerCase()) || blog.type.toString().toLowerCase().includes(search.toLowerCase()) || blog.email.toString().toLowerCase().includes(search.toLowerCase())
+
+      }).map(blog => (
   <>
   <tr className="blog-preview" key={blog.id}>
     <td><Link to={`/blogs/${blog.id}`}>
@@ -17,7 +25,9 @@ const BlogList = ({ blogs }) => {
     </td>
     <td>{blog.age }</td>
     <td>{blog.email }</td>
-    <td>{blog.type }</td>
+    <td>
+    {blog.type }
+    </td>
   </tr>
   </>
 
